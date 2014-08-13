01c574391a59ec00c3a43e8edd2cbc9a9c93f1fe
Tim Perry <tim@forepoint.co.uk>
2014-08-13 10:04:31 +0100

    removing the setup scripts folder

--- setup_scripts/cleanup_wp.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-rm -rf ../public_html/wp/{wp-content,license.txt,readme.html,wp-config-sample.php}
--- setup_scripts/generate_salts.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-curl -o ../config/salts.php https://api.wordpress.org/secret-key/1.1/salt/
